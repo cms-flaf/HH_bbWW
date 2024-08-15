ea5c2a119b022d413795e90dec3c29ca9f242dfc
Artem Bolshov <abolshov@cern.ch>
2024-08-15 22:39:43 +0200

    add tools for reporting purity of samples

---
+++ studies/purity/PurityChecker.py
@@ -0,0 +1,60 @@
+import uproot
+import numpy as np
+import vector
+import awkward as ak
+
+
+class PurityChecker:
+    def __init__(self, sort_by):
+        self.sort_by = sort_by if 'centralJet_' in sort_by else 'centralJet_' + sort_by
+
+
+    def ComputePurity(self, file_name, channel):
+        file = uproot.open(file_name)
+        tree = file['Events']
+        branches = tree.arrays()
+
+        jet_hadrFlav = branches['centralJet_hadronFlavour']
+        jet_trueBjetTag = branches['centralJet_TrueBjetTag']
+        jet_btag = branches[self.sort_by]
+
+        has_at_least_2_reco_bjets = (ak.count_nonzero(jet_hadrFlav == 5, axis=1) >= 2)
+
+        reco_lep1_type = branches['lep1_type']
+        reco_lep2_type = branches['lep2_type']
+
+        gen_lep1_type = branches['lep1_genLep_kind']
+        gen_lep2_type = branches['lep2_genLep_kind']
+
+        reco_lep1_mu = reco_lep1_type == 1
+        reco_lep1_ele = reco_lep1_type == 0
+
+        reco_lep2_mu = reco_lep2_type == 1
+        reco_lep2_ele = reco_lep2_type == 0
+
+        gen_lep1_mu = (gen_lep1_type == 2) | (gen_lep1_type == 4)
+        gen_lep1_ele = (gen_lep1_type == 1) | (gen_lep1_type == 3)
+
+        gen_lep2_mu = (gen_lep2_type == 2) | (gen_lep2_type == 4)
+        gen_lep2_ele = (gen_lep2_type == 1) | (gen_lep2_type == 3)
+
+        lep1_correct = (reco_lep1_mu & gen_lep1_mu) | (reco_lep1_ele & gen_lep1_ele)
+        lep2_correct = (reco_lep2_mu & gen_lep2_mu) | (reco_lep2_ele & gen_lep2_ele)
+
+        corr_lep_reco = lep1_correct
+        if channel == 'dl':
+            corr_lep_reco = corr_lep_reco & lep2_correct
+
+        presel = has_at_least_2_reco_bjets & corr_lep_reco
+
+        jet_btag = jet_btag[presel]
+        jet_trueBjetTag = jet_trueBjetTag[presel]
+
+        sort_by_btag = ak.argsort(jet_btag)[::, ::-1]
+        jet_btag = jet_btag[sort_by_btag]
+        jet_trueBjetTag = jet_trueBjetTag[sort_by_btag]
+
+        both_are_b_jets = (jet_trueBjetTag[:, 0]) & (jet_trueBjetTag[:, 1])
+        purity = ak.count_nonzero(both_are_b_jets)/ak.count(both_are_b_jets)
+
+        return purity
