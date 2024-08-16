d5ef037a7cd1593c07ba863bca35b0ddce335400
Artem Bolshov <abolshov@cern.ch>
2024-08-17 00:04:48 +0200

    implement configuring neural net from yaml file

--- Studies/NeuralNetEstimator/Reco/test_JetNet.py
+++ Studies/NeuralNetEstimator/Reco/test_JetNet.py
@@ -1,6 +1,7 @@
-from JetNet import JetNet
+from Common.JetNet import JetNet
 from Common.JetNetData import JetNetData
 from Common.JetNet_utils import PlotPrediction
+import yaml 
 
 
 def main():
@@ -9,15 +10,19 @@ def main():
     labels = ['H_VV_px', 'H_VV_py', 'H_VV_pz', 'H_VV_E', 'X_mass']
 
     input_files = ["test_GluGlutotoRadiontoHHto2B2Vto2B2JLNu_450.root"]
+    
+    with open("config.yaml", 'r') as stream:
+        cfg = yaml.safe_load(stream)
 
-    data = JetNetData(features, labels)
-    data.ReadFiles(input_files)
+        data = JetNetData(features, labels)
+        data.ReadFiles(input_files)
 
-    net = JetNet(features, labels)
-    net.LoadModel("./models/JetNet_v2.keras")
+        net = JetNet(features, labels, cfg)
+        # net.LoadModel("./models/JetNet_v2.keras")
+        net.LoadModel("./models/nn_estimator_v1.keras")
 
-    pred = net.Predict(data.data[features])
-    mass = PlotPrediction(pred, data.data)
+        pred = net.Predict(data.data[features])
+        mass = PlotPrediction(pred, data.data)
     
     
 if __name__ == '__main__':
