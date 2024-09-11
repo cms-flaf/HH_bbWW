eab2d9d045e3e501e5107ed38fb455a55ec72776
Artem Bolshov <abolshov@cern.ch>
2024-09-11 03:12:53 +0200

    update DataWrapper

--- Studies/NeuralNetEstimator/Common/DataWrapper.py
+++ Studies/NeuralNetEstimator/Common/DataWrapper.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import awkward as ak
 import uproot
 import vector
 from sklearn.utils import shuffle
@@ -24,9 +25,8 @@ class DataWrapper():
         tree = file[self.tree_name]
         branches = tree.arrays()
 
-        # data_dict = {name: np.array(branches[name]) for name in self.features + self.labels + self.extra_data}
-        d1 = {name: np.array(branches[name]) for name in extra_data}
-        d1["X_mass"] = ak.to_numpy(branches['X_mass'])
+        d1 = {name: np.array(branches[name]) for name in self.extra_data}
+        d1["X_mass"] = np.array(branches['X_mass'], dtype=float)
 
         centralJet_p4 = vector.zip({'pt': branches['centralJet_pt'], 
                                     'eta': branches['centralJet_eta'], 
@@ -35,13 +35,14 @@ class DataWrapper():
 
         d2 = {}
         for i in range(self.n_jets):
-            d2[f"centralJet{i}_px"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.px[:, :n_jets], n_jets), 0.0))[:, i]
-            d2[f"centralJet{i}_py"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.py[:, :n_jets], n_jets), 0.0))[:, i]
-            d2[f"centralJet{i}_pz"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.pz[:, :n_jets], n_jets), 0.0))[:, i]
-            d2[f"centralJet{i}_E"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.E[:, :n_jets], n_jets), 0.0))[:, i]
-            d2[f"centralJet{i}_btagPNetB"] = ak.to_numpy(ak.fill_none(ak.pad_none(branches['centralJet_btagPNetB'][:, :n_jets], n_jets), 0.0))[:, i] 
-            d2[f"centralJet{i}_PNetRegPtRawCorrNeutrino"] = ak.to_numpy(ak.fill_none(ak.pad_none(branches['centralJet_PNetRegPtRawCorrNeutrino'][:, :n_jets], n_jets), 0.0))[:, i] 
-            d2[f"centralJet{i}_PNetRegPtRawRes"] = ak.to_numpy(ak.fill_none(ak.pad_none(branches['centralJet_PNetRegPtRawRes'][:, :n_jets], n_jets), 0.0))[:, i] 
+            d2[f"centralJet{i}_px"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.px[:, :self.n_jets], self.n_jets), 0.0))[:, i]
+            d2[f"centralJet{i}_py"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.py[:, :self.n_jets], self.n_jets), 0.0))[:, i]
+            d2[f"centralJet{i}_pz"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.pz[:, :self.n_jets], self.n_jets), 0.0))[:, i]
+            d2[f"centralJet{i}_E"] = ak.to_numpy(ak.fill_none(ak.pad_none(centralJet_p4.E[:, :self.n_jets], self.n_jets), 0.0))[:, i]
+            d2[f"centralJet{i}_btagPNetB"] = ak.to_numpy(ak.fill_none(ak.pad_none(branches['centralJet_btagPNetB'][:, :self.n_jets], self.n_jets), 0.0))[:, i] 
+            d2[f"centralJet{i}_PNetRegPtRawCorr"] = ak.to_numpy(ak.fill_none(ak.pad_none(branches['centralJet_PNetRegPtRawCorr'][:, :self.n_jets], self.n_jets), 0.0))[:, i]
+            d2[f"centralJet{i}_PNetRegPtRawCorrNeutrino"] = ak.to_numpy(ak.fill_none(ak.pad_none(branches['centralJet_PNetRegPtRawCorrNeutrino'][:, :self.n_jets], self.n_jets), 0.0))[:, i]  
+            d2[f"centralJet{i}_PNetRegPtRawRes"] = ak.to_numpy(ak.fill_none(ak.pad_none(branches['centralJet_PNetRegPtRawRes'][:, :self.n_jets], self.n_jets), 0.0))[:, i] 
 
         HVV_p4 = vector.zip({'pt': branches['genHVV_pt'],
                             'eta': branches['genHVV_eta'],
@@ -56,7 +57,7 @@ class DataWrapper():
         data_dict = d1 | d2
 
         df = pd.DataFrame.from_dict(data_dict)
-        print(f"Reading {file_name}, containing {df.shape[0]} entries")
+        print(f"Reading {df.shape[0]} entries from {file_name}")
         self.data = pd.concat([self.data, df]) 
 
 
@@ -70,14 +71,22 @@ class DataWrapper():
 
 
     def TrainTestSplit(self):
-        train_df = self.data.sample(frac=self.train_frac, random_state=42)
-        test_df = self.data.drop(train_df.index)
+        self.Shuffle()
+        train_df = self.SelectEvents(0, 2)
+        test_df = self.SelectEvents(1, 2)
 
+        test_df = test_df.drop(self.labels, axis=1)
+        
+        self.train_events = train_df['event']
+        self.test_events = test_df['event']
+
+        test_df = test_df.drop(['event'], axis=1)
+        train_df = train_df.drop(['event'], axis=1)
+       
         self.train_features = train_df[self.features]
         self.test_features = test_df[self.features]
         self.train_labels = train_df[self.labels]
-        self.test_labels = test_df[self.labels]
 
 
     def SelectEvents(self, value, modulo):
-        self.selected_events = self.data[self.data['event'] % modulo == value]
+        return self.data[self.data['event'] % modulo == value]
