5cd068150af7c1d4092a81eb91f936048d8f7d51
Artem Bolshov <abolshov@cern.ch>
2024-09-12 21:36:12 +0200

    update plotting function

--- Studies/NeuralNetEstimator/Common/JetNet_utils.py
+++ Studies/NeuralNetEstimator/Common/JetNet_utils.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# out_* - output of the neural net
+# target_* - true vaules of quantities that nn predicts
 @tf.function
 def GetMXPred(out_px, out_py, out_pz, target_px, target_py, target_pz, target_E, target_mass):
     H_mass = 125.0
@@ -32,40 +33,14 @@ def PlotLoss(history):
     plt.clf()
 
 
-def PlotPrediction(pred_labels, test_labels):
-    # for predicting H->bb take H->VV from test labels
-    # for predicting H->VV take H->bb from test labels
+def PlotPrediction(label_df, predicted_df):
+    # label_df contains p4 of H->WW
+    # predicted_df contains p4 of H->bb 
 
-    H_VV_px = test_labels['H_VV_px']
-    H_VV_py = test_labels['H_VV_py']
-    H_VV_pz = test_labels['H_VV_pz']
-    H_VV_E = test_labels['H_VV_E']
-
-    # H_WW_px = pred_labels[:, 0]
-    # H_WW_py = pred_labels[:, 1]
-    # H_WW_pz = pred_labels[:, 2]
-    # H_mass = 125.0
-    # H_WW_E_sqr = H_mass*H_mass + H_WW_px*H_WW_px + H_WW_py*H_WW_py + H_WW_pz*H_WW_pz
-    # H_WW_E = np.sqrt(H_WW_E_sqr)
-
-    # H_bb_px = test_labels['H_bb_px']
-    # H_bb_py = test_labels['H_bb_py']
-    # H_bb_pz = test_labels['H_bb_pz']
-    # H_bb_E = test_labels['H_bb_E']
-
-    # H_bb_px_true = test_labels['genbjet1_px'] + test_labels['genbjet2_px']
-    # H_bb_py_true = test_labels['genbjet1_py'] + test_labels['genbjet2_py']
-    # H_bb_pz_true = test_labels['genbjet1_pz'] + test_labels['genbjet2_pz']
-    # H_bb_E_true = test_labels['genbjet1_E'] + test_labels['genbjet2_E']
-
-    H_bb_px = pred_labels[:, 0]
-    H_bb_py = pred_labels[:, 1]
-    H_bb_pz = pred_labels[:, 2]
-    H_mass = 125.0
-    H_bb_E_sqr = H_mass*H_mass + H_bb_px*H_bb_px + H_bb_py*H_bb_py + H_bb_pz*H_bb_pz
-    H_bb_E = np.sqrt(H_bb_E_sqr)
-
-    X_mass_pred = np.sqrt((H_bb_E + H_VV_E)**2 - (H_bb_px + H_VV_px)**2 - (H_bb_py + H_VV_py)**2 - (H_bb_pz + H_VV_pz)**2)
+    X_mass_pred = np.sqrt((label_df['H_VV_E'] + predicted_df['H_bb_E'])**2 -
+                          (label_df['H_VV_px'] + predicted_df['H_bb_px'])**2 -
+                          (label_df['H_VV_py'] + predicted_df['H_bb_py'])**2 -
+                          (label_df['H_VV_pz'] + predicted_df['H_bb_pz'])**2)
 
     width = PredWidth(X_mass_pred)
     peak = PredPeak(X_mass_pred)
@@ -79,8 +54,6 @@ def PlotPrediction(pred_labels, test_labels):
     plt.grid(True)
     plt.savefig('JetNet_prediction.pdf', bbox_inches='tight')
     plt.clf()
-
-    return X_mass_pred
 
 
 def PredWidth(pred_mass):
