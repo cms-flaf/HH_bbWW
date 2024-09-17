7937d9f1862cd732bcd12c09e860e4e2939371e2
Artem Bolshov <abolshov@cern.ch>
2024-09-17 17:35:22 +0200

    update EstimatorTools

--- Studies/RandomSamplingEstimator/include/EstimatorTools.hpp
+++ Studies/RandomSamplingEstimator/include/EstimatorTools.hpp
@@ -9,7 +9,7 @@
 #include "TRandom3.h"
 #include "TH1.h"
 
-using OptionalPair = std::optional<std::pair<double, double>>;
+#include "Definitions.hpp"
 
 OptionalPair JetRescFact(TLorentzVector& j1, TLorentzVector& j2, std::unique_ptr<TH1F>& pdf, double mass);
 std::optional<TLorentzVector> ComputeNu(TLorentzVector const& l, TLorentzVector const& j1, TLorentzVector const& j2, TLorentzVector const& met, double mh, double eta);
