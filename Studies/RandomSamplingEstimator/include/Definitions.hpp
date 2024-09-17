e13d6873eefd0266cb19dd82e79934c90a8d3391
Artem Bolshov <abolshov@cern.ch>
2024-09-17 17:33:03 +0200

    move all usings to a dedicated header

---
+++ Studies/RandomSamplingEstimator/include/Definitions.hpp
@@ -0,0 +1,10 @@
+#ifndef DEFS_HPP
+#define DEFS_HPP
+
+using RVecD = ROOT::VecOps::RVec<double>;
+using RVecS = ROOT::VecOps::RVec<size_t>;
+using RVecLV = ROOT::VecOps::RVec<TLorentzVector>;
+
+using OptionalPair = std::optional<std::pair<double, double>>;
+
+#endif
