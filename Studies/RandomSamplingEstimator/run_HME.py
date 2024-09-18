1f221fdf2be463754fa28f59c0550fd56ca0d40e
Artem Bolshov <abolshov@cern.ch>
2024-09-18 18:03:22 +0200

    add hme output for two possible cases

--- Studies/RandomSamplingEstimator/run_HME.py
+++ Studies/RandomSamplingEstimator/run_HME.py
@@ -1,5 +1,6 @@
 import ROOT
 import argparse
+import numpy as np
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
 
     ROOT.gROOT.SetBatch(True)
     ROOT.EnableImplicitMT(8)
+
     ROOT.gROOT.ProcessLine('#include "include/EstimatorTools.hpp"')
     ROOT.gROOT.ProcessLine('#include "include/KinTools.hpp"')
     ROOT.gROOT.ProcessLine('#include "include/CombTools.hpp"')
@@ -18,6 +20,7 @@ def main():
 
     ROOT.gInterpreter.Declare("""auto file_pdf = std::make_unique<TFile>("pdf.root", "READ");""")
     ROOT.gInterpreter.Declare("""auto pdf = std::unique_ptr<TH1F>(static_cast<TH1F*>(file_pdf->Get("pdf")));""")
+    ROOT.gInterpreter.Declare("""TRandom3 rg; rg.SetSeed(42);""")
 
     df = ROOT.RDataFrame("Events", input_file)
 
@@ -52,7 +55,7 @@ def main():
                                          res.push_back(light_jets[best_onshell_pair.second]);
                                          res.push_back(reco_lep_p4);
                                          res.push_back(reco_met_p4);
-                                         return res;""")
+                                         return res; """)
 
     df = df.Define("input_offshell", """ RVecLV res;
                                          res.push_back(reco_bj1_p4);
@@ -61,15 +64,28 @@ def main():
                                          res.push_back(light_jets[best_offshell_pair.second]);
                                          res.push_back(reco_lep_p4);
                                          res.push_back(reco_met_p4);
-                                         return res;""")
+                                         return res; """)
 
-    # df = df.Define("test", "Test(input_onshell)")
-    df = df.Define("test", "UseHist(pdf)")
-    test = df.AsNumpy(["test"])
-    print(test["test"])
+    df = df.Define("hme_onshell", """ auto hme = EstimateMass(input_onshell, pdf, rg, event);
+                                      if (hme)
+                                      {{
+                                          auto [mass, succ_rate] = hme.value();
+                                          return mass;
+                                      }}
+                                      return -1.0; """)
 
-    print(f"dataset contains {df.Count().GetValue()} events")
-    
+    df = df.Define("hme_offshell", """ auto hme = EstimateMass(input_offshell, pdf, rg, event);
+                                       if (hme)
+                                       {{
+                                           auto [mass, succ_rate] = hme.value();
+                                           return mass;
+                                       }}
+                                       return -1.0; """)
+
+    result = df.AsNumpy(["hme_onshell", "hme_offshell"])
+    output = np.column_stack((np.array(result["hme_onshell"]), np.array(result["hme_offshell"])))
+    np.savetxt("hme_output.txt", output, delimiter=' ', fmt='%10.5f')
+
 
 if __name__ == '__main__':
     main()
