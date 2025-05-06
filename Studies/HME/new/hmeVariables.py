acf3fbc691cb03bf09ebb463393ff0e3ffcbd865
Artem Bolshov <abolshov@cern.ch>
2025-05-06 19:32:39 +0200

    modify HME return type and remove filtering events to apply HME to entire dataset

--- Studies/HME/new/hmeVariables.py
+++ Studies/HME/new/hmeVariables.py
@@ -1,5 +1,6 @@
 import ROOT
 ROOT.gROOT.ProcessLine('#include "include/EstimatorLTWrapper.hpp"')
+ROOT.gROOT.ProcessLine('#include "include/Constants.hpp"')
 
 
 def GetHMEVariables(df, channel):
@@ -23,12 +24,18 @@ def GetHMEVariables(df, channel):
     df = df.Define("met", """HME::LorentzVectorF_t res(PuppiMET_pt, 0.0, PuppiMET_phi, 0.0);    
                           return res;""")
 		
-    df = df.Define("hme_mass", f"""auto const& hme = HME::EstimatorLTWrapper::Instance().GetEstimator().EstimateMass(jets, leptons, met, event, HME::Channel::{channel});
-                                Float_t mass = -1.0f;
-                                if (hme.has_value())
-                                {{
-                                    auto const& result_array = hme.value();
-                                    mass = result_array[static_cast<size_t>(HME::EstimOut::mass)];
-                                }}
-                                return mass;""")                      
+    df = df.Define("hme_output", f"""if (has_necessary_inputs)
+                                    {{
+                                        auto const& hme = HME::EstimatorLTWrapper::Instance().GetEstimator().EstimateMass(jets, leptons, met, event, HME::Channel::{channel});
+                                        if (hme.has_value())
+                                        {{
+                                            return hme.value();
+                                        }}
+                                    }}
+                                    std::array<Float_t, HME::ESTIM_OUT_SZ> hme_res{{}};
+                                    for (size_t i = 0; i < HME::ESTIM_OUT_SZ; ++i)
+                                    {{
+                                        hme_res[i] = -1.0f;
+                                    }}
+                                    return hme_res;""")                      
     return df
