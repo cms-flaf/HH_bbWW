cad335c51817310fe3f67cdf32106bfa320755ed
abolshov <100383407+abolshov@users.noreply.github.com>
2025-07-18 08:26:26 -0500

    HME split (#30)
    * split HME into SingleLepHME and DoubleLepHME
    
    * add jet area to list of saved branches
    
    * include tagger probabilities in HME
    
    * aggregate results from different combinations to single histogram
    
    * improve SL HME code
    
    * update constants
    
    * reduce N_ITER
    
    * add channel name in HME branches to avoid overwriting
    
    * Revert "reduce N_ITER"
    
    This reverts commit 483d0d0c1a31d53a2e1954dfcaceb43a4b8edd38.
    
    * undo changes
    
    * add channel checks
    
    * update FLAF
    
    * add payload_name to producer constructor
    
    * add new HME variables to plotting config
    
    * updaing FLAF
    
    ---------
    
    Co-authored-by: Artem Bolshov <abolshov@cern.ch>

--- Analysis/PayloadProducers.py
+++ Analysis/PayloadProducers.py
@@ -1,23 +1,28 @@
 from Studies.HME.new.hmeVariables import GetHMEVariables
 
 class HMEProducer:
-    def __init__(self, cfg):
+    def __init__(self, cfg, payload_name):
         self.cfg = cfg
+        self.payload_name = payload_name
 
     def run(self, dfw):
         if "ncentralJet" not in dfw.df.GetColumnNames():
             dfw.Define("ncentralJet", "return centralJet_pt.size();")
-        if self.cfg['channel'] == "DL":
+
+        ch = self.cfg['channel']
+        if ch == "DL":
             dfw.Define("has_necessary_inputs", "ncentralJet >= 2 && lep1_pt > 0.0 && lep2_pt > 0.0")
-        elif self.cfg['channel'] == "SL":
+        elif ch == "SL":
             dfw.Define("has_necessary_inputs", "ncentralJet >= 4 && lep1_pt > 0.0")
-        
-        dfw.df = GetHMEVariables(dfw.df, self.cfg['channel'])
+        else:
+            raise RuntimeError(f"Illegal channel in config: {ch}")
+
+        dfw.df = GetHMEVariables(dfw.df, ch)
         for col in self.cfg['columns']:
             if col != 'valid':
-                dfw.DefineAndAppend(f"HME_{col}", f"return hme_output[static_cast<size_t>(HME::EstimOut::{col})];")
+                dfw.DefineAndAppend(f"{self.payload_name}_{col}", f"return hme_output[static_cast<size_t>(HME::EstimOut::{col})];")
         if 'valid' in self.cfg['columns']:
-            dfw.DefineAndAppend("HME_valid", "return HME_mass > 0.0;")
+            dfw.DefineAndAppend(f"{self.payload_name}_valid", f"return {self.payload_name}_mass > 0.0;")
         return dfw
 
 class DNNProducer:
