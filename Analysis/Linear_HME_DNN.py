e6bd75f198d6c4ddb9e272b2c837faba9c924a9e
Devin <aebid@tamu.edu>
2025-07-28 17:12:13 +0200

    Update linearizer to be dfw based, add variable into plots and config

--- Analysis/Linear_HME_DNN.py
+++ Analysis/Linear_HME_DNN.py
@@ -1,17 +1,4 @@
-#python3 /afs/cern.ch/work/p/prsolank/private/FLAF_8thJan/AnaProd/NNInterface.py --inModelDir /afs/cern.ch/work/p/prsolank/private/FLAF_8thJan/config/HH_bbtautau/nn_models --inFile /tmp/prsolank/luigi-tmp-416131263.root --outFileName /tmp/prsolank/luigi-tmp-862152055.root --uncConfig /afs/cern.ch/work/p/prsolank/private/FLAF_8thJan/config/Run2_2018/weights.yaml --globalConfig /afs/cern.ch/work/p/prsolank/private/FLAF_8thJan/config/HH_bbtautau/global.yaml --EraName e2018 --Mass 400 --Spin 2 --PairType 2
-
 from __future__ import annotations
-import os, sys
-import numpy as np
-import awkward as ak
-import onnxruntime as ort
-import psutil
-import yaml
-import os
-import ROOT
-import FLAF.Common.Utilities as Utilities
-import Analysis.hh_bbww as analysis
-
 
 class LinHMEDNN:
     def __init__(self, cfg, payload_name):
@@ -19,47 +6,16 @@ class LinHMEDNN:
         self.cfg = cfg
         self.payload_name = payload_name
 
-        self.HME_coarse_bins = [-10, 250, 280, 300, 350, 400, 500, 600, 800, 10_000]
-
-        # What to save in tmp file
-        self.vars_to_save = set(['FullEventId', 'DoubleLepHME_mass', 'DNN_M0_Signal'])
-
-    def run(self, array):
-        print("Running LinHMEDNN producer")
-
-        hme = array['DoubleLepHME_mass']
-        dnn = array['DNN_M0_Signal']
-
-        def find_index_between(num):
-            lst = self.HME_coarse_bins
-            for i in range(len(lst) - 1):
-                if lst[i] <= num < lst[i + 1]:
-                    return i
-            return None
-
-        hme_binned = [ find_index_between(hme_val) for hme_val in hme ]
-        linear_hme_dnn = hme_binned + dnn
-
-        array['linear_hme_dnn'] = np.array(linear_hme_dnn, dtype=np.float32)
-
-        # Delete not-needed branches
-        for col in array.fields:
-            if col not in self.cfg['columns']:
-                if col != 'FullEventId':
-                    del array[col]
-                    
-        # Rename the branches
+        self.hme_bins = "-10, 250, 280, 300, 350, 400, 500, 600, 800, 10000"
+        self.n_bins = 9
+        
+    def run(self, dfw):
+        dfw.Define("linear_hme_dnn", f"""
+        const Double_t bins[{self.n_bins+1}] = {{{self.hme_bins}}};
+        TAxis *axis = new TAxis({self.n_bins}, bins);
+        return axis->FindBin(DoubleLepHME_mass) + DNN_M0_Signal;
+        """)
         for col in self.cfg['columns']:
-            if col in array.fields:
-                array[f"{self.payload_name}_{col}"] = array[f"{col}"]
-                del array[f"{col}"]
-            else:
-                print(f"Expected column {col} not found in your payload array!")
-
-        print("Finished my linHMEDNN, columns are")
-        print(self.cfg['columns'])
-        print("And array is ")
-        print(array)
-
-        return array
+            dfw.DefineAndAppend(f"{self.payload_name}_{col}", f"return {col};")
+        return dfw
 
