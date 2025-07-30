72b19f19e93d4aa5a7f3cf02209bedf1d9b37616
Devin <aebid@tamu.edu>
2025-07-30 23:18:40 +0200

    Update linearize code to work with parametric dnn

--- Analysis/Linear_HME_DNN.py
+++ Analysis/Linear_HME_DNN.py
@@ -6,15 +6,20 @@ class LinHMEDNN:
         self.cfg = cfg
         self.payload_name = payload_name
 
-        self.hme_bins = "-10, 250, 280, 300, 350, 400, 500, 600, 800, 10000"
-        self.n_bins = 9
+        self.hme_bins = [-10, 250, 280, 300, 350, 400, 500, 600, 800, 10000]
+        self.n_bins = len(self.hme_bins) - 1
+        self.hme_bins_string = ','.join(map(str, self.hme_bins))
         
     def run(self, dfw):
-        dfw.Define("linear_hme_dnn", f"""
-        const Double_t bins[{self.n_bins+1}] = {{{self.hme_bins}}};
-        TAxis *axis = new TAxis({self.n_bins}, bins);
-        return axis->FindBin(DoubleLepHME_mass) + DNN_M0_Signal;
-        """)
+        for col in self.cfg['columns']:
+            print(col)
+            mass = col.split('_')[-1][1:]
+            print(mass)
+            dfw.Define(f"linear_hme_dnn_m{mass}", f"""
+            static const Double_t bins[{self.n_bins+1}] = {{{self.hme_bins_string}}};
+            static TAxis axis({self.n_bins}, bins);
+            return axis.FindBin(DoubleLepHME_mass) + DNNParametric_M{mass}_Signal;
+            """)
         for col in self.cfg['columns']:
             dfw.DefineAndAppend(f"{self.payload_name}_{col}", f"return {col};")
         return dfw
