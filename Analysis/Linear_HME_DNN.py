7ddbca370504a5cacbe95ad7f6b51cb785d65139
Devin <aebid@tamu.edu>
2025-07-30 23:57:10 +0200

    Format robot wanted me to remove invisible spaces -_-

--- Analysis/Linear_HME_DNN.py
+++ Analysis/Linear_HME_DNN.py
@@ -7,40 +7,40 @@ class LinHMEDNN:
         self.payload_name = payload_name
     
         self.hme_bins = [
-            -10, 
-            200, 
-            225, 
-            275, 
-            325, 
-            375, 
-            425, 
-            475, 
-            525, 
-            575, 
-            625, 
-            675, 
-            750, 
-            850, 
-            950, 
-            1100, 
-            1300, 
-            1500, 
-            1700, 
-            1900, 
-            2250, 
-            2750, 
-            3500, 
-            4500, 
+            -10,
+            200,
+            225,
+            275,
+            325,
+            375,
+            425,
+            475,
+            525,
+            575,
+            625,
+            675,
+            750,
+            850,
+            950,
+            1100,
+            1300,
+            1500,
+            1700,
+            1900,
+            2250,
+            2750,
+            3500,
+            4500,
             10000
         ]
         self.n_bins = len(self.hme_bins) - 1
         self.hme_bins_string = ",".join(map(str, self.hme_bins))
-        
+
     def run(self, dfw):
         for col in self.cfg["columns"]:
             mass = col.split("_")[-1][1:]
             dfw.Define(
-                f"linear_hme_dnn_m{mass}", 
+                f"linear_hme_dnn_m{mass}",
                 f"""
             static const Double_t bins[{self.n_bins+1}] = {{{self.hme_bins_string}}};
             static TAxis axis({self.n_bins}, bins);
