74f05f7e89ca283c67c52e01de2949f013855002
Devin <aebid@tamu.edu>
2025-07-30 23:50:37 +0200

    Update formatting, new bot doesn't like single quotes -- change hme bins to be around our parametric mass points

--- Analysis/Linear_HME_DNN.py
+++ Analysis/Linear_HME_DNN.py
@@ -5,22 +5,21 @@ class LinHMEDNN:
 
         self.cfg = cfg
         self.payload_name = payload_name
-
-        self.hme_bins = [-10, 250, 280, 300, 350, 400, 500, 600, 800, 10000]
+    
+        self.hme_bins = [-10, 200, 225, 275, 325, 375, 425, 475, 525, 575, 625, 675, 750, 850, 950, 1100, 1300, 1500, 1700, 1900, 2250, 2750, 3500, 4500, 10000]
         self.n_bins = len(self.hme_bins) - 1
-        self.hme_bins_string = ','.join(map(str, self.hme_bins))
+        self.hme_bins_string = ",".join(map(str, self.hme_bins))
         
     def run(self, dfw):
-        for col in self.cfg['columns']:
-            print(col)
-            mass = col.split('_')[-1][1:]
-            print(mass)
+        for col in self.cfg["columns"]:
+            mass = col.split("_")[-1][1:]
             dfw.Define(f"linear_hme_dnn_m{mass}", f"""
             static const Double_t bins[{self.n_bins+1}] = {{{self.hme_bins_string}}};
             static TAxis axis({self.n_bins}, bins);
             return axis.FindBin(DoubleLepHME_mass) + DNNParametric_M{mass}_Signal;
-            """)
-        for col in self.cfg['columns']:
+            """,
+            )
+        for col in self.cfg["columns"]:
             dfw.DefineAndAppend(f"{self.payload_name}_{col}", f"return {col};")
         return dfw
 
