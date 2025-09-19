02adce0fb59a28f3f702f545a84da706c4a4f30f
Artem Bolshov <abolshov@cern.ch>
2025-09-19 20:44:13 +0200

    start implementing functionality of DeepHMEProducer

--- Analysis/DeepHMEProducer.py
+++ Analysis/DeepHMEProducer.py
@@ -1,11 +1,15 @@
 import numpy as np
+import Analysis.hh_bbww as analysis
 
 class DeepHMEProducer:
-    def __init__(self):
-        pass
+    def __init__(self, cfg, payload_name):
+        self.cfg = cfg
+        self.payload_name = payload_name
     
     def prepare_dfw(self, dfw):
-        pass
+        dfw.df = analysis.defineAllP4(dfw.df)
+        dfw.df = analysis.AddDeepHMEVariables(dfw.df)
+        return dfw
 
     def run(self):
         pass
