74715c16d8bec21dded934eca31313737e5355c6
Devin Aebi <45836403+aebid@users.noreply.github.com>
2026-05-30 15:12:20 -0500

    Non parametric dnn support (#79)
    * Add new vars to bbww, redo DNN Application to handle non-parametric models
    
    * Add copy and rename func for DNN models to the config folder
    
    * Add new non-parametric model
    
    * yaml formating by hand
    
    * Update DNN to produce logit form
    
    * Update the DNN producer to include both single and double DNN, as well as a total DNN var
    
    * Update histograms to use general shared signal, add some SL/DL points to validate
    
    * Cleaned hists more
    
    * Update HME to be done in one cache
    
    * Save multiple classes output to the cache
    
    * Removed old DNN defs
    
    * Update plots and global with new payloads
    
    * Update custom ci yaml for re-adding DNN
    
    * Add DNN plot configs
    
    * Bugfix for parametric, but without a model it is hard to verify
    
    * Formatting
    
    * Update Corrections
    
    * Update FLAF

--- Analysis/DNN_Application.py
+++ Analysis/DNN_Application.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 import os, sys
+import gc
 import numpy as np
 import awkward as ak
 import onnxruntime as ort
@@ -21,74 +22,67 @@ class DNNProducer:
         self.period = period
 
         sys.path.append(os.environ["ANALYSIS_PATH"])
-        ROOT.gROOT.ProcessLine(".include " + os.environ["ANALYSIS_PATH"])
-        ROOT.gInterpreter.Declare(f'#include "FLAF/include/Utilities.h"')
-        ROOT.gROOT.ProcessLine(f'#include "FLAF/include/HistHelper.h"')
-        ROOT.gROOT.ProcessLine(f'#include "FLAF/include/AnalysisTools.h"')
-        ROOT.gROOT.ProcessLine(f'#include "FLAF/include/AnalysisMath.h"')
-        ROOT.gROOT.ProcessLine(f'#include "FLAF/include/MT2.h"')
-        ROOT.gROOT.ProcessLine(f'#include "FLAF/include/Lester_mt2_bisect.cpp"')
 
-        self.dnnConfig = {}
-        dnnFolder = os.path.join(
-            os.environ["ANALYSIS_PATH"], "config", "DNN", self.cfg["version"]
-        )
-        with open(os.path.join(dnnFolder, "dnn_config.yaml"), "r") as file:
-            self.dnnConfig = yaml.safe_load(file)
-
-        # Features to use for DNN application (single vals)
-        features = self.dnnConfig["features"]
-        # Features to use for DNN application (vectors and index)
-        list_features = self.dnnConfig["listfeatures"]
-        # Features to use for DNN application (high level names to create)
-        highlevel_features = self.dnnConfig["highlevelfeatures"]
-        # Features to use for DNN application (hme names to pull from cache)
-        hme_features = (
-            self.dnnConfig["hmefeatures"]
-            if "hmefeatures" in self.dnnConfig.keys()
-            else None
-        )
-
-        # Features to load from df to awkward
         load_features = set()
-        if features != None:
-            load_features.update(features)
-        if list_features != None:
-            for feature in list_features:
-                load_features.update([feature[0]])
-        if highlevel_features != None:
-            load_features.update(highlevel_features)
-        if hme_features != None:
-            load_features.update(hme_features)
+        columns_to_save = set()
 
-        load_features.update(["FullEventId"])
-        load_features.update(["event"])
+        self.cfg_dict = {
+            "DL": self.cfg.get("DL", None),
+            "SL": self.cfg.get("SL", None),
+        }
+
+        self.models = {}
+        self.masses = self.cfg.get("masses")
+        self.dnnConfig = {}
+
+        for channel, cfg in self.cfg_dict.items():
+            if cfg == None:
+                print(f"Channel {channel} does not have DNN defined, skip.")
+                continue
+            self.models[channel] = {}
+            self.dnnConfig[channel] = {}
+            parametric = cfg.get("parametric", False)
+
+            dnnFolder = os.path.join(
+                os.environ["ANALYSIS_PATH"], "config", "DNN", cfg["version"]
+            )
+
+            for mass in self.masses:
+                if parametric:
+                    this_mass_folder = dnnFolder
+                    mass = 0
+                    # Set mass to 0 since parametric should have the same config each time
+                    # This helps the mass loop happening in the ApplyDNN func
+
+                else:
+                    this_mass_folder = os.path.join(dnnFolder, f"m{mass}")
+                file_name = os.path.join(this_mass_folder, "dnn_config.yaml")
+                with open(file_name, "r") as file:
+                    self.dnnConfig[channel][f"m{mass}"] = yaml.safe_load(file)
+
+                load_features.update(self.dnnConfig[channel][f"m{mass}"]["features"])
+
+                modelname_parity = self.dnnConfig[channel][f"m{mass}"][
+                    "modelname_parity"
+                ]
+                self.dnnConfig[channel][f"m{mass}"]["model_paths"] = [
+                    [f"{os.path.join(this_mass_folder, x)}.onnx", y]
+                    for x, y in modelname_parity
+                ]
+
+        columns_to_save.update(
+            [f"{self.payload_name}_{col}" for col in self.cfg["columns"]]
+        )
 
         # What to save in tmp file
+        load_features.update(["FullEventId", "event", "SL", "DL"])
         self.vars_to_save = load_features
-        # What to save for final output
-        self.cols_to_save = [
-            f"{self.payload_name}_{col}" for col in self.cfg["columns"]
-        ]
-
-        modelname_parity = self.dnnConfig["modelname_parity"]
-
-        self.models = [
-            [ort.InferenceSession(f"{os.path.join(dnnFolder, x)}.onnx"), y]
-            for x, y in modelname_parity
-        ]
-
-    def prepare_dfw(self, dfw, dataset):
-        print("Running DNN preparer")
-
-        dfw.df = analysis.defineAllP4(dfw.df)
-        dfw.df = analysis.AddDNNVariables(dfw.df)
-        return dfw
 
     def run(self, array):
         print("Running DNN producer")
 
         array = self.ApplyDNN(array)
+        array = self.selectDNN(array)
 
         # Delete not-needed branches
         for col in array.fields:
@@ -103,140 +97,142 @@ class DNNProducer:
                 del array[f"{col}"]
             else:
                 print(f"Expected column {col} not found in your payload array!")
+                print(f"Available columns were {array.fields}")
 
         return array
 
     def ApplyDNN(self, branches):
-        models = self.models
-        dnnConfig = self.dnnConfig
+        output_fields = {}
 
-        # Features to use for DNN application (single vals)
-        features = dnnConfig["features"]
-        # Features to use for DNN application (vectors and index)
-        list_features = dnnConfig["listfeatures"]
-        # Features to use for DNN application (high level names to create)
-        highlevel_features = dnnConfig["highlevelfeatures"]
-        # Features to use for DNN application (hme names to pull from cache)
-        hme_features = (
-            self.dnnConfig["hmefeatures"]
-            if "hmefeatures" in self.dnnConfig.keys()
-            else None
-        )
+        for channel, all_dnnConfig in self.dnnConfig.items():
+            if all_dnnConfig == None:
+                print(f"Channel {channel} does not have DNN defined, skip.")
+                continue
 
-        nClasses = dnnConfig["nClasses"] if "nClasses" in dnnConfig.keys() else 3
-        nParity = dnnConfig["nParity"] if "nParity" in dnnConfig.keys() else 4
+            masses = self.masses if ("m0" not in all_dnnConfig.keys()) else [0]
+            for mass in masses:
+                dnnConfig = all_dnnConfig[f"m{mass}"]
+                models = dnnConfig["model_paths"]
 
-        use_parametric = dnnConfig["use_parametric"]
-        # param_mass_list = [ 250, 260, 270, 280, 300, 350, 450, 550, 600, 650, 700, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2500, 3000, 4000, 5000 ]
-        param_mass_list = dnnConfig["parametric_list"]
+                features = dnnConfig["features"]
 
-        class_names_list = (
-            dnnConfig["class_names"]
-            if "class_names" in dnnConfig.keys()
-            else ["Signal", "TT", "DY"]
-        )
+                nClasses = (
+                    dnnConfig["nClasses"] if "nClasses" in dnnConfig.keys() else 3
+                )
+                nParity = dnnConfig["nParity"] if "nParity" in dnnConfig.keys() else 4
 
-        if not use_parametric:
-            param_mass_list = [0]
+                use_parametric = dnnConfig["use_parametric"]
+                param_mass_list = dnnConfig["parametric_list"]
 
-        nEvents = len(branches)
-        print(f"Running DNN Over {nEvents} events")
-
-        # Initialize the final predictions per parametric per event per class
-        all_predictions = np.zeros((len(param_mass_list), nEvents, nClasses))
-
-        event_number = branches.event
-
-        array = np.array(
-            [getattr(branches, feature_name) for feature_name in features]
-        ).transpose()
-
-        # Get vector value array
-        default_value = 0.0
-        if list_features != None:
-            array_listfeatures = np.array(
-                [
-                    ak.fill_none(
-                        ak.pad_none(getattr(branches, feature_name), index + 1),
-                        default_value,
-                    )[:, index]
-                    for [feature_name, index] in list_features
-                ]
-            ).transpose()
-            # Need to append the value features and the listfeatures together
-            array = np.append(array, array_listfeatures, axis=1)
-
-        # Need to append the high level features and the other features together
-        if highlevel_features != None:
-            array_highlevelfeatures = np.array(
-                [getattr(branches, feature_name) for feature_name in highlevel_features]
-            ).transpose()
-            array = np.append(array, array_highlevelfeatures, axis=1)
-
-        if hme_features != None:
-            array_hmefeatures = np.array(
-                [getattr(branches, feature_name) for feature_name in hme_features]
-            ).transpose()
-            array = np.append(array, array_hmefeatures, axis=1)
-
-        # Initialize the local predictions including per parity, but this will be summed out later
-        local_predictions = np.zeros(
-            (len(param_mass_list), len(array), nParity, nClasses)
-        )
-
-        for parityIdx, [sess, parityfunc] in enumerate(models):
-            # We want to only apply the 3 models that are NOT trained on this parity
-
-            # Add parametric mass point to the array
-            for param_idx, param_mass in enumerate(param_mass_list):
-                param_array = np.array([[param_mass for x in array]]).transpose()
-                if use_parametric:
-                    final_array = np.append(array, param_array, axis=1)
-                else:
-                    final_array = array
-
-                # prediction = model.predict(final_array)
-                prediction = sess.run(
-                    None, {"x": final_array}
-                )  # Take only first entry, prediction is [ [Sig, TT, DY], [mBB_SR] ]
-
-                class_prediction = prediction[0]
-                # adv_prediction = prediction[1] # Sometimes we don't use an adv model
-
-                # Now we need to set the trained parity to 0
-                # But if there is only one model, then skip parity
-                event_num = np.expand_dims(
-                    event_number, axis=-1
-                )  # We now get event_number from the FullEventId branch earlier
-                parity_filter = np.repeat(event_num, nClasses, axis=-1)
-                if nParity != 1:
-                    class_prediction = np.where(
-                        parity_filter % nParity != parityIdx, class_prediction, 0.0
-                    )
-                local_predictions[param_idx, :, parityIdx, :] = class_prediction
-
-        # Reduce dimension by sum the parity axis from local to global
-        all_predictions = np.sum(local_predictions, axis=2)
-
-        if nParity != 1:
-            all_predictions = all_predictions / (
-                nParity - 1
-            )  # So we want to divide by nParity-1 (4 parity -> train with 1, apply with remaining 3)
-
-        # Last save the branches
-        for param_idx, param_mass in enumerate(param_mass_list):
-            this_param_prediction = all_predictions[
-                param_idx, :, :
-            ]  # Now we want to get the individual param masses predictions for filling
-
-            for class_idx, class_name in enumerate(class_names_list):
-                branches[f"M{param_mass}_{class_name}"] = (
-                    this_param_prediction.transpose()[class_idx].astype(np.float32)
+                class_names_list = (
+                    dnnConfig["class_names"]
+                    if "class_names" in dnnConfig.keys()
+                    else ["Signal", "TT", "DY"]
                 )
 
-        print("Finishing call, memory?")
-        process = psutil.Process(os.getpid())
-        mem_mb = process.memory_info().rss / 1024 / 1024
-        print(f"Current memory usage: {mem_mb:.2f} MB")
+                nEvents = len(branches)
+                print(f"Running DNN Over {nEvents} events")
 
+                event_number = np.asarray(branches.event)
+                if nParity != 1:
+                    event_mod = event_number % nParity
+
+                array = np.stack(
+                    [
+                        np.asarray(getattr(branches, feature_name), dtype=np.float32)
+                        for feature_name in features
+                    ],
+                    axis=1,
+                )
+
+                if use_parametric:
+                    final_array = np.empty(
+                        (nEvents, array.shape[1] + 1), dtype=np.float32
+                    )
+                    final_array[:, :-1] = array
+
+                all_predictions = np.zeros(
+                    (len(param_mass_list), nEvents, nClasses), dtype=np.float32
+                )
+
+                for parityIdx, [onnx_name, parityfunc] in enumerate(models):
+                    sess = ort.InferenceSession(onnx_name)
+                    for param_idx, param_mass in enumerate(param_mass_list):
+                        if use_parametric:
+                            final_array[:, -1] = param_mass
+                            input_array = final_array
+                        else:
+                            input_array = array
+
+                        prediction = sess.run(None, {"x": input_array})
+                        class_prediction = np.asarray(prediction[0], dtype=np.float32)
+
+                        if nParity != 1:
+                            mask = event_mod != parityIdx
+                            class_prediction[~mask, :] = 0.0
+
+                        all_predictions[param_idx] += class_prediction
+                        del prediction, class_prediction
+
+                if nParity != 1:
+                    all_predictions /= nParity - 1
+
+                # Last save the branches
+                for param_idx, param_mass in enumerate(param_mass_list):
+                    this_param_prediction = all_predictions[param_idx, :, :]
+                    this_param_prediction_logit = np.clip(
+                        this_param_prediction, 1e-7, 1 - 1e-7
+                    )
+                    this_param_prediction_logit = np.log(
+                        this_param_prediction_logit / (1 - this_param_prediction_logit)
+                    )
+
+                    for class_idx, class_name in enumerate(class_names_list):
+                        field_name = f"{channel}_M{param_mass}_{class_name}"
+                        output_fields[field_name] = this_param_prediction_logit[
+                            :, class_idx
+                        ].copy()
+
+                if use_parametric:
+                    del final_array
+                del array
+                if nParity != 1:
+                    del event_mod
+                del all_predictions
+                gc.collect()
+                print("Finishing call, memory?")
+                process = psutil.Process(os.getpid())
+                mem_mb = process.memory_info().rss / 1024 / 1024
+                print(f"Current memory usage: {mem_mb:.2f} MB")
+
+        for field_name, values in output_fields.items():
+            branches[field_name] = values
+
+        del output_fields
+
+        return branches
+
+    def selectDNN(self, branches):
+        # Here we will take SL and DL and choose which branch to save as final column
+        output_fields = {}
+
+        classes_to_save = ["Signal", "TT", "DY", "ST"]
+
+        for mass in self.masses:
+            for class_name in classes_to_save:
+                field_name = f"M{mass}_{class_name}"
+                # Build the empty branches with ones
+                if f"SL_{field_name}" not in branches.fields:
+                    branches[f"SL_{field_name}"] = np.zeros_like(branches.event)
+                if f"DL_{field_name}" not in branches.fields:
+                    branches[f"DL_{field_name}"] = np.zeros_like(branches.event)
+                output_fields[field_name] = np.where(
+                    branches.SL,
+                    branches[f"SL_{field_name}"],
+                    branches[f"DL_{field_name}"],
+                )
+
+        for field_name, values in output_fields.items():
+            branches[field_name] = values
+        del output_fields
         return branches
