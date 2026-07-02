9850211113f0604f8f69042b6ac8fa384b92a848
Devin <aebid@tamu.edu>
2026-07-02 20:40:22 +0200

    Add 2024 structure for the CI sanity check, fixed logic in 2B2Tau signal variables check

--- AnaProd/anaTupleDef.py
+++ AnaProd/anaTupleDef.py
@@ -621,8 +621,8 @@ def addAllVariables(
         )
         dfw.colToSave.extend(hltBranches)
 
-    if isSignal and (
-        ("XtoHHto2B2Tau" not in dataset_cfg["process_name"])
-        or ("XtoHHto2Tau2B" not in dataset_cfg["process_name"])
+    if isSignal and not (
+        ("XtoHHto2B2Tau" in dataset_cfg["process_name"])
+        or ("XtoHHto2Tau2B" in dataset_cfg["process_name"])
     ):
         defineSignalVariables(dfw)
