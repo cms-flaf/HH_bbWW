3ca1efc95f917c91586b0a218896a234bdbe722e
Devin <aebid@tamu.edu>
2026-07-18 03:33:21 +0200

    Remove fullres from the histTupleProducer

--- Analysis/histTupleDef.py
+++ Analysis/histTupleDef.py
@@ -37,11 +37,6 @@ def GetDfw(df, setup, dataset_name):
     isData = dataset_name == "data"
     period = global_params["era"]
     dfw = analysis.DataFrameBuilderForHistograms(df, global_params, period)
-    new_dfw = analysis.PrepareDfForHistograms(dfw, isData)
-
-    for var in setup.histTuple_fullres_vars:
-        new_dfw.colToSave.append(var)
-
     return new_dfw
 
 
